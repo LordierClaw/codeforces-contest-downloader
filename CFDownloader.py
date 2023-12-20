a4e08dcd9a001bf07c23ac7d6f21639514f8a657
Hưng <hung.nguyen30@onemount.com>
2023-12-20 15:13:09 +0700

    feat: add forward proxy to avoid ratelimit, increase sleep time

--- CFDownloader.py
+++ CFDownloader.py
@@ -1,6 +1,7 @@
 import mechanize
 from http.cookiejar import CookieJar
 import time
+from urllib.parse import quote
 
 from lxml import html
 from lxml.html import html5parser as etree
@@ -16,6 +17,13 @@ class CFDownloader:
         self.__browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
     def retryMechanize(self, url, timeout):
+        proxyurl = "https://api.scrapingant.com/v2/general?url="
+        #convert url encode to http encoding
+        encoded_url = quote(url)
+        proxyurl += encoded_url
+        proxyurl += "&x-api-key=718a20a35d174225904cf787680ddc6c"
+
+
         error_occured = False
         try: 
             self.__browser.open(url, timeout=timeout)
@@ -23,12 +31,12 @@ class CFDownloader:
             error_occured = True
         while error_occured:
             try:
-                self.__browser.open(url, timeout=timeout)
+                self.__browser.open(proxyurl, timeout=timeout)
                 error_occured = False
             except mechanize.URLError:
                 error_occured = True
-                print("Error occured, retrying after 5 seconds...")
-                time.sleep(5)
+                print("Error occured, retrying after 30 seconds...")
+                time.sleep(30)
         
 
     def login(self, handle, password):
@@ -40,10 +48,15 @@ class CFDownloader:
         self.__browser.submit()
     
     def getSourceCode(self, groupId, contestId, submissionId):
-        url = f"https://codeforces.com/group/{groupId}/contest/{contestId}/submission/{submissionId}"
-        self.retryMechanize(url, 10)
-        tree2 = html.fromstring(self.__browser.response().read())
-        data = etree.fromstring(str(pq(tree2.xpath('//*[@id=\"program-source-text\"]')))).text
-        data = data.replace("<pre id=\"program-source-text\" class=\"prettyprint lang-cpp linenums program-source\" style=\"padding: 0.5em;\">","")
+        data = ""
+        try:
+            url = f"https://codeforces.com/group/{groupId}/contest/{contestId}/submission/{submissionId}"
+            self.retryMechanize(url, 10)
+            tree2 = html.fromstring(self.__browser.response().read())
+            data = etree.fromstring(str(pq(tree2.xpath('//*[@id=\"program-source-text\"]'))))
+            data = data.text
+            data = data.replace("<pre id=\"program-source-text\" class=\"prettyprint lang-cpp linenums program-source\" style=\"padding: 0.5em;\">","")
+        except:
+            data = ""
         return data
     
